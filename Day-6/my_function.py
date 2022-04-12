614c96fa8bd5e1cfb41876853f42f1213c42845d
parzival92 <abhishdevops@gmail.com>
2022-04-13 01:30:14 +0530

    Day-6

---
+++ Day-6/my_function.py
@@ -0,0 +1,5 @@
+def my_function():
+  print("Hello")
+  print("function")
+
+my_function()
---
+++ Day-6/while.py
@@ -0,0 +1,26 @@
+while True:
+    print("xxxxxxxxxxxxxxxxxxx") # infinite loop
+    break # Break to come out loop
+
+
+# While loops are dangerous as it can go infinite loop if conition remain true always
+
+# Take 10 integers from keyboard using loop and print their average value on the screen.
+
+#sum = 0
+#i=10
+#while i>0:
+   # num = int(input())
+ #   sum += num
+ #   i -=1
+ 
+#avg = sum/10
+#print(avg)
+
+# Print multiplication table of 24, 50 and 29 using loop.
+
+i=1
+while i<=10:
+    result = 24 *i
+    print(f"24 multiply {i} = {result}")
+    i+=1
