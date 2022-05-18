2fe393bbfaa7b757d228d42d53d180fb171592c6
parzival92 <abhishdevops@gmail.com>
2022-05-19 00:34:25 +0530

    Snake Final and Inheritance

---
+++ Day 021/slicing_list.py
@@ -0,0 +1,12 @@
+piano_key = ['a','b','c','d','e','f','g']
+print(piano_key[2:5])
+print(piano_key[2:])
+print(piano_key[:4])
+# Every second item
+print(piano_key[2::2])
+# Reverse list
+print(piano_key[::-1])
+
+# Tuples
+piano_tup = ('mi','fa','so','ti','yi','su','li')
+print(piano_tup[2:5])
